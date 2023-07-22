ce0c80fabeef29a36e59cb5ebe23c3cf67e8cac5
martine701 <allietmartine@gmail.com>
2023-07-22 18:57:45 +0100

    commit sat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, CarComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, CarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
